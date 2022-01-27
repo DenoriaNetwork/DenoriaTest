674d7c9b0923133cc1d155c19cb602e18da2c3a3
Skogrine <mathieumalaj.dev@gmail.com>
2022-01-27 14:24:11 +0100

    refactor: Change VsCode project to Intellij IDEA project

---
+++ src/fr/denoria/test/DenoTest.java
@@ -0,0 +1,26 @@
+package fr.denoria.test;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DenoTest extends JavaPlugin {
+
+  public static DenoTest instance;
+
+  @Override
+  public void onEnable() {
+    instance = this;
+    super.onEnable();
+  }
+
+  @Override
+  public void onDisable() {
+    // TODO Auto-generated method stub
+    super.onDisable();
+  }
+
+  public static DenoTest getInstance() {
+    return instance;
+  }
+
+
+}
